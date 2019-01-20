a662d8648268d1d711c4002bfe72383ed7eced85
soragui <ziwo520@gmail.com>
2019-01-20 10:25:13 +0800

    restore main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
 
 import 'package:flutter/material.dart';
 
-import 'widget/ShoppingListItem.dart';
-import 'widget/ShoppingList.dart';
-
-import 'widget/container.dart';
-import 'widget/rowcolumn.dart';
-import 'widget/images.dart';
-import 'widget/text.dart';
-import 'widget/snackbars.dart';
-import 'widget/tabbar.dart';
-
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
@@ -18,24 +8,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Shopping App',
-      /*
-        home: new ShoppingList(
-        products: <Product> [
-          new Product(name: 'Eggs'),
-          new Product(name: 'Flour'),
-          new Product(name: 'Chocolate chips'),
-          new Product(name: 'Some Salad')
-        ],
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Fluter'),
+        ),
+
+        body: Center(
+          child: Text('Hello, Flutter'),
+        ),
       ),
-      */
-      theme: ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.green,
-        accentColor: Colors.cyan[900],
-        fontFamily: 'ChelaOne',
-      ),
-      home: new TabBarE(),
 
     );
   }
